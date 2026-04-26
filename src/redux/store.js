7e9fa2d62bbb96368793b4469c55b0ad40539665
kickbell <dev.kickbell@gmail.com>
2026-04-27 04:24:19 +0900

    fix: Redux store에 forumReducer 등록

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import postReducer from '@/post/postSlice';
 import userReducer from '@/user/userSlice';
 import authReducer from '@/auth/authSlice';
+import forumReducer from '@/forum/forumSlice';
 
 export const store = configureStore({
   reducer: {
     post: postReducer,
     user: userReducer,
     auth: authReducer,
+    forum: forumReducer,
   },
 });
